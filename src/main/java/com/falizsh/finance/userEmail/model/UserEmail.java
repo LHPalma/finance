0cfe53832f4265faf064ef1fa42c38e4e94f357f
Luiz Palma <luizpalma16@gmail.com>
2025-12-16 21:56:07 -0300

    Add UserEmail constructors

--- src/main/java/com/falizsh/finance/userEmail/model/UserEmail.java
+++ src/main/java/com/falizsh/finance/userEmail/model/UserEmail.java
@@ -6,13 +6,17 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Entity(name = "UserEmail")
 @Table(name = "user_email")
