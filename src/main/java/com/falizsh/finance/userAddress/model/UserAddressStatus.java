ee9a16ddb4afc149bb52095bc97fc581d05c927b
Luiz Palma <luizpalma16@gmail.com>
2025-12-24 17:10:03 -0300

    add displayName and fromString() to user addresses enumsn

--- src/main/java/com/falizsh/finance/userAddress/model/UserAddressStatus.java
+++ src/main/java/com/falizsh/finance/userAddress/model/UserAddressStatus.java
@@ -2,9 +2,23 @@ package com.falizsh.finance.userAddress.model;
 
 public enum UserAddressStatus {
 
-    ACTIVE,
-    INACTIVE,
-    OUTDATED,
-    INVALID;
+    ACTIVE("Ativo"),
+    INACTIVE("Inativo"),
+    OUTDATED("Desatualizado"),
+    INVALID("Inválido");
 
+    private final String displayName;
+
+    UserAddressStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public static UserAddressStatus fromString(String text) {
+        for (UserAddressStatus status : UserAddressStatus.values()) {
+            if (status.name().equalsIgnoreCase(text)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("invalid.user.address.status");
+    }
 }
--- src/main/java/com/falizsh/finance/userAddress/model/UserAddressType.java
+++ src/main/java/com/falizsh/finance/userAddress/model/UserAddressType.java
@@ -2,8 +2,23 @@ package com.falizsh.finance.userAddress.model;
 
 public enum UserAddressType {
 
-    RESIDENTIAL,
-    COMMERCIAL,
-    PROFESSIONAL;
+    RESIDENTIAL("Residencial"),
+    COMMERCIAL("Comercial"),
+    PROFESSIONAL("Profissinal");
+
+    private final String displayName;
+
+    UserAddressType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public static UserAddressType fromString(String text) {
+        for (UserAddressType type : UserAddressType.values()) {
+            if (type.name().equalsIgnoreCase(text)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("invalid.user.address.type");
+    }
 
 }
