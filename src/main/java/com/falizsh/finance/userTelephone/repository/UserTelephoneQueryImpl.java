2e8e3d63757179256c1f689a5fd5ea57c7cc3ace
Luiz Palma <luizpalma16@gmail.com>
2025-12-27 17:34:52 -0300

    fix(UserTelephone): repositoy -> repository

--- src/main/java/com/falizsh/finance/userTelephone/repository/UserTelephoneQueryImpl.java
+++ src/main/java/com/falizsh/finance/userTelephone/repository/UserTelephoneQueryImpl.java
@@ -1,5 +1,6 @@
 package com.falizsh.finance.userTelephone.repository;
 
+import com.falizsh.finance.userTelephone.dto.UserTelephoneCreateRequest;
 import com.falizsh.finance.userTelephone.model.UserTelephone;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -12,21 +13,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserTelephoneQueryImpl implements UserTelephoneQuery {
 
-    private final UserTelephoneRepository repositoy;
+    private final UserTelephoneRepository repository;;
 
     @Override
     public Page<UserTelephone> findAll(Pageable pageable) {
-        return repositoy.findAll(pageable);
+        return repository.findAll(pageable);
     }
 
     @Override
     public Page<UserTelephone> findByUserId(Long userId, Pageable pageable) {
-        return repositoy.findByUserId(userId, pageable);
+        return repository.findByUserId(userId, pageable);
     }
 
     @Override
     public Page<UserTelephone> findByAreaCode(String areaCode, Pageable pageable) {
-        return repositoy.findByAreaCode(areaCode, pageable);
+        return repository.findByAreaCode(areaCode, pageable);
     }
+
 
 }
