d24410dc938eecb294887facf32258aeb0b6b139
Luiz Palma <luizpalma16@gmail.com>
2025-12-30 21:36:21 -0300

    build: Add Spring Cloud OpenFeign support

--- src/main/java/com/falizsh/finance/FinanceApplication.java
+++ src/main/java/com/falizsh/finance/FinanceApplication.java
@@ -2,12 +2,15 @@ package com.falizsh.finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class FinanceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinanceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceApplication.class, args);
+    }
 
 }
