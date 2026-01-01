4e53100621115e225992a78729a3a6793afeda2a
Luiz Palma <luizpalma16@gmail.com>
2026-01-01 19:31:20 -0300

    Add Feign client for Brapi API integration

---
+++ src/main/java/com/falizsh/finance/shared/brapiAPIAdapter/BrapiClient.java
@@ -0,0 +1,26 @@
+package com.falizsh.finance.shared.brapiAPIAdapter;
+
+
+import com.falizsh.finance.shared.brapiAPIAdapter.dto.BrapiQuoteResponse;
+import com.falizsh.finance.shared.brapiAPIAdapter.dto.BrapiResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "brapiClient", url = "${brapi.url:https://brapi.dev/api}")
+public interface BrapiClient {
+
+    @GetMapping("v2/currency")
+    BrapiResponse getCurrency(
+            @RequestParam("currency") String currencyPair,
+            @RequestParam("token") String token
+    );
+
+    @GetMapping("/quote/{tickers}")
+    BrapiQuoteResponse getQuote(
+            @PathVariable String tickers,
+            @RequestParam("token") String token
+    );
+
+}
