7ae11cb6972f3ffbae3d20e62e434f02e2765c97
Luiz Palma <luizpalma16@gmail.com>
2026-01-04 01:16:45 -0300

    Add CopomMeeting Model

---
+++ src/main/java/com/falizsh/finance/economics/model/CopomMeeting.java
@@ -0,0 +1,60 @@
+package com.falizsh.finance.economics.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "CompomMeeting")
+@Table(name = "copom_meeting")
+public class CopomMeeting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate meetingDate;
+
+    private Integer meetingNumber;
+
+    private String description;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal previousSelicTarget;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal selicTarget;
+
+    private String minutesUrl;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    public BigDecimal getVariation() {
+        if (selicTarget == null || previousSelicTarget == null) {
+            return BigDecimal.ZERO;
+        }
+
+        return selicTarget.subtract(previousSelicTarget);
+    }
+
+
+}
