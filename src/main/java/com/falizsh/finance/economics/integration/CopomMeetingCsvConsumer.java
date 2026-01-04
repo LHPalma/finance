01bb51bafd55b65c64cd62b0d6770686617f7919
Luiz Palma <luizpalma16@gmail.com>
2026-01-04 20:48:50 -0300

    Update CopomMeetingCsvConsumer.java

--- src/main/java/com/falizsh/finance/economics/integration/CopomMeetingCsvConsumer.java
+++ src/main/java/com/falizsh/finance/economics/integration/CopomMeetingCsvConsumer.java
@@ -1,6 +1,24 @@
+package com.falizsh.finance.economics.integration;
+
+import com.falizsh.finance.economics.repository.CopomMeetingCsvReader;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
+public class CopomMeetingCsvConsumer {
+
+    private final CopomMeetingCsvReader reader;
+
+    @Async
+    @EventListener
+    @Transactional
+    public void onReceiveMessage(CopomMeetingCsvUploadEvent event) {
+        reader.process(event.getFilePath());
+    }
+}
